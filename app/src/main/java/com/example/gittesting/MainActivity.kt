b431d6bd2844d4c46d0d94aba72989d21984b8e6
sumanth <sumanth2174@gmail.com>
2020-05-27 10:46:53 +0530

    branch 2

--- app/src/main/java/com/example/gittesting/MainActivity.kt
+++ app/src/main/java/com/example/gittesting/MainActivity.kt
@@ -4,9 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
+    var y = 2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        y = y++
     }
 }
